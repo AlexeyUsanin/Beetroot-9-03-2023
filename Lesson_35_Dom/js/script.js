cdcc8c221c16ec4465c8abc01e25cee83f99768d
Oleksii Usanin <aleksej.usanin@icloud.com>
2023-06-01 21:50:26 +0300

    DOM: styles, attributes, events

--- Lesson_35_Dom/js/script.js
+++ Lesson_35_Dom/js/script.js
@@ -1,7 +1,7 @@
 // const heading = document.getElementsByClassName('heading');
 // const list = document.getElementById('list');
 // const listItem = document.getElementsByTagName('li');
-const heading = document.querySelector('.heading');
+const heading = document.querySelector('.js-heading');
 const headings = document.querySelectorAll('.heading');
 //
 
@@ -21,7 +21,7 @@ console.log('heading :>> ', heading);
 const tasks = [
   {
     name: 'Зробити ДЗ 1',
-    completed: false,
+    completed: true,
     deadline: '1/06/2023'
   },
   {
@@ -37,17 +37,98 @@ h3.innerHTML = '<p>Heading 3</p>';
 
 console.log('h3 :>> ', h3);
 
+// list.insertAdjacentHTML('afterbegin', `<li>${item.name}</li>`);
+// list.insertAdjacentHTML('afterend', `<li>${item.name}</li>`);
+// list.insertAdjacentHTML('beforebegin', `<li>${item.name}</li>`);
+// list.insertAdjacentHTML('beforeend', `<li>${item.name}</li>`);
 
-const list = document.querySelector('#list');
 
-for (const item of tasks) {
-  const listItem = document.createElement('li');
-  listItem.innerText = item.name;
+function renderListItem(item) {
+  const listItem = document.createElement('li');  // li
+  const checkbox = document.createElement('input'); // input[type="checkbox"]
+  const button = document.createElement('button');
+  const listItemText = document.createElement('span');  // li
 
-  // list.insertAdjacentHTML('afterbegin', `<li>${item.name}</li>`);
-  // list.insertAdjacentHTML('afterend', `<li>${item.name}</li>`);
-  // list.insertAdjacentHTML('beforebegin', `<li>${item.name}</li>`);
-  // list.insertAdjacentHTML('beforeend', `<li>${item.name}</li>`);
+  checkbox.setAttribute('type', 'checkbox');
 
-  list.append(listItem);
+  button.innerText = 'Delete';
+  listItemText.innerText = `Name: ${item.name} | Deadline: ${item.deadline}`;
+
+  // listItem.style.margin = '10px';
+  listItem.classList.add('list__item');
+  checkbox.classList.add('list__item-checkbox');
+  button.classList.add('list__item-button');
+
+  listItem.prepend(checkbox);
+  // listItem.append(checkbox);
+  listItem.append(listItemText);
+  listItem.append(button);
+
+  checkbox.onchange = function () {
+    console.log('this :>> ', this);
+    console.log('checkbox changed', )
+
+    // console.log('listItem.classList.contains', listItem.classList.contains('completed'));
+
+    if (this.checked) {
+      listItem.classList.add('completed');
+    } else {
+      listItem.classList.remove('completed');
+    }
+  }
+
+  button.onclick = function () {
+    // const el = this.parentElement;
+    this.closest('.list__item').remove()
+    // this.parentNode.nextElementSibling // next element
+    // this.parentNode.prevElementSibling // prev element
+
+
+    // console.log('el :>> ', el);
+
+    // el.remove()
+  }
+
+
+  if (item.completed) {
+    listItem.classList.add('completed');
+    checkbox.checked = true;
+  }
+
+  console.log('listItem :>> ', listItem);
+  return listItem;
+}
+
+function renderTaskList(tasks) {
+  const list = document.querySelector('#list');
+
+  for (const item of tasks) {
+    const listItem = renderListItem(item);
+
+    list.append(listItem);
+  }
+}
+
+renderTaskList(tasks);
+
+
+const form = document.querySelector('#form');
+
+form.onsubmit = function (event) {
+  event.preventDefault();
+  console.log('Submit');
+
+  const taskName = document.querySelector('#task-name');
+  const taskDate = document.querySelector('#task-date');
+
+  console.log('taskName.value :>> ', taskName.value);
+  console.log('taskDate.value :>> ', taskDate.value);
+
+  tasks.push({
+    name: taskName.value,
+    deadline: taskDate.value,
+    completed: false
+  })
+
+  console.log('tasks :>> ', tasks);
 }
